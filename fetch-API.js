fb65b487152aa882b72b941c441ded0ccfbd256e
ozgur <ozgur.ersoz3@gmail.com>
2023-08-25 18:07:22 +0300

    fapi-options

--- fetch-API.js
+++ fetch-API.js
@@ -21,3 +21,20 @@ fetch("http://api.github.com/users/ersozo/repos")
     });
     document.body.appendChild(div);
   });
+
+// fetch options are given in an object: method, body, headers
+
+function createPost({ title, body }) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({ title, body }),
+    headers: {
+      "Content-Type": "application/json",
+      token: "abc123",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+createPost({ title: "My POst", body: "This is my post" });
